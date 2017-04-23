293b87142267083bb97fe5f2f0ce182bf991aa5e
andreiciontea <ciontea@mcmaster.ca>
2017-04-22 18:56:44 -0700

    code

--- comboscript.js
+++ comboscript.js
@@ -10,8 +10,42 @@ function check() {
 			var v5=document.getElementById("Tenofovir Disoproxil Fumarate");
 			var v6=document.getElementById("Zidovudine");
 			
-			var c2 = 2;
-			alert("does this work")
-			return false;
+			var c1 = 0;
+			var c2 = 0;
+			var c3 = 0;
+			var c4 = 0;
+			var c5 = 0;
+			var c6 = 0;
+			var ct = 0;
+			
+			
+			if (v1.checked == true){
+				var c1 = 1;
+			}
+			if (v2.checked == true){
+				var c2 = 1;
+			}
+			if (v3.checked == true){
+				var c3 = 1;
+			}
+			if (v4.checked == true){
+				var c4 = 1;
+			}
+			if (v5.checked == true){
+				var c5 = 1;
+			}
+			if (v6.checked == true){
+				var c6 = 1;
+			}
+			
+			var ct = c1+c2+c3+c4+c5+c6;
+			
+			if (ct < 2){
+				alert("choose two nrtis")
+				return false;
+			}
+			
+			
+			
 			
 		}
